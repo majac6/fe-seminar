41aed2cb20cc5b6f2ed7203da2979bd0a4f3b3eb
HungSun LIM <majac6@gmail.com>
2024-01-19 07:04:31 +0900

    기본 프로젝트들 셋업

---
+++ react-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import {Helmet} from "react-helmet";
+
+
+function App() {
+  return (<div className="App">
+    <Helmet>
+      <meta charSet="utf-8"/>
+      <title>My Title</title>
+      <link rel="canonical" href="http://mysite.com/example"/>
+    </Helmet>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>);
+}
+
+export default App;
